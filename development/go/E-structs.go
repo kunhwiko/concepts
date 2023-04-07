a9798be66aed16db01720a7f60929a19f5b59c2d
Kun Hwi Ko <kokunhwi@gmail.com>
2023-04-07 14:06:52 -0700

    Add clarification to diff between functions and methods

--- development/go/E-structs.go
+++ development/go/E-structs.go
@@ -8,13 +8,14 @@ import (
 // Go allows for the creation of new types
 type names []string
 
+// This is a "function" that does not belong to a particular type
 func declareType() {
     var friends names = []string{"Alice", "Bob"}
     friends = names{"Cassandra", "Ellie"}
     friends.modifyFirst()
 }
 
-// This is a method that has a receiver of type 'names' called 'n'
+// This is a "method" to a given type that has a receiver of type 'names' called 'n'
 // By convention, the instance of the type is represented as a single letter
 func (n names) modifyFirst() {
     n[0] = "Alice"
